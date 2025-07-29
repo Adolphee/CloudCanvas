b855528ffbabbc3c42c69f3cc0530d9f85f0d706
Adolphe <a-jhay@hotmail.com>
2025-07-29 22:46:22 +0200

    Now returning model-binding entire Metadata objects instead of just URLs

--- CloudCanvas.Web/Models/GalleryViewModel.cs
+++ CloudCanvas.Web/Models/GalleryViewModel.cs
@@ -6,7 +6,6 @@ namespace CloudCanvas.Web.Models
     public class GalleryViewModel
     {
         [BindProperty]
-        public required List<GalleryItemDTO> BlobsMetadata { get; set; }
-        public required List<string> BlobUrls { get; set; }
+        public required List<BlobMetaDTO> Blobs { get; set; }
     }
 }
