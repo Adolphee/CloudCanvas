15c4d596ce988e2735deda1c36f44e516b79a6b2
Adolphe <a-jhay@hotmail.com>
2025-07-30 22:47:34 +0200

    Added table view for gallery items, with delete & edit buttons + edit form (modal)

--- CloudCanvas.Web/wwwroot/js/gallery.js
+++ CloudCanvas.Web/wwwroot/js/gallery.js
@@ -8,6 +8,7 @@ var carousel = document.querySelector('.carousel-inner');
         if (!response.ok) throw new Error(`Error: ${response.status}`);
         const metadata = await response.json();
         updateGalleryItems(metadata);
+        configureEditModals(metadata);
     } catch (error) {
         // TODO: better error handling; alert, prompt for page reload or JS retry
         console.error("Failed to fetch metadata:", error.message);
@@ -59,4 +60,27 @@ function updateCarouselItemMetadata(viewCard, cardMeta) {
 
     var description = viewCard.querySelector('.carousel-description');
     description.textContent = cardMeta.description || 'No Description';
+}
+
+function configureEditModals(metadata) {
+    metadata.forEach(blob => {
+        const modal = document.getElementById(`${blob.identifier}-edit-gallery-modal`)
+        if (modal) {
+            modal.addEventListener('show.bs.modal', event => {
+                // Button that triggered the modal
+                const button = event.relatedTarget
+                // Extract info from data-bs-* attributes
+                const recipient = button.getAttribute('data-bs-whatever')
+                // If necessary, you could initiate an Ajax request here
+                // and then do the updating in a callback.
+
+                // Update the modal's content.
+                const modalTitle = modal.querySelector('.modal-title')
+                const modalBodyInput = modal.querySelector('.modal-body input')
+
+                modalTitle.textContent = `New message to ${recipient}`
+                modalBodyInput.value = recipient
+            })
+        }
+    });
 }
