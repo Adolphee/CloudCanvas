1600dfd765f17d6e50aa614eed56e3a974cbf079
Adolphe <a-jhay@hotmail.com>
2025-07-31 00:22:47 +0200

    extracted 2 reusable partial views from (Gallery)Index.cshtml:  Carousel & GalleryCard

---
+++ CloudCanvas.Web/Views/Shared/_GalleryCard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CloudCanvas.Web.Views.Shared
+{
+    public class _GalleryCardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
