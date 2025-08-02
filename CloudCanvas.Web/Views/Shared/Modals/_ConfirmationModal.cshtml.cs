bbadb1db3cab9ce8a7b2f0c36e75fc91cd294ba7
Adolphe <a-jhay@hotmail.com>
2025-08-03 01:02:39 +0200

    used the more modern <partial> tag for synchronous DOM rendering

---
+++ CloudCanvas.Web/Views/Shared/Modals/_ConfirmationModal.cshtml.cs
@@ -0,0 +1,11 @@
+﻿namespace CloudCanvas.Web.Views.Gallery.Modals
+{
+    public class _ConfirmationModel
+    {
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public string Footer { get; set; }
+        public string BlobId { get; set; }
+
+    }
+}
