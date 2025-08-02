4183a0214053bc4196e04fba1efd14746566f60c
Adolphe <a-jhay@hotmail.com>
2025-08-03 00:47:49 +0200

    move DTO.OriginalFilename property, BlobMeta -> GalleryItem: more relevant for frontend

--- CloudCanvas.Shared/DTOs/GalleryItemDTO.cs
+++ CloudCanvas.Shared/DTOs/GalleryItemDTO.cs
@@ -7,6 +7,8 @@ namespace CloudCanvas.Shared.DTOs
     {
         [Required]
         public string Url { get; set; } = default!;
+        [Required]
+        public string OriginalFilename { get; set; } = default!;
         public Dictionary<ThumbnailSize, string> Thumbnails { get; set; } = new(); // BlobUrls for the thumbnails
         public string? UploadedBy { get; set; }
         public DateTimeOffset LastModified { get; set; }
