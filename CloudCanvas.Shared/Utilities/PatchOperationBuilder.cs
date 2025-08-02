a58c849b69ecc6268be8be88522fbd212c8fd3a3
Adolphe <a-jhay@hotmail.com>
2025-08-03 00:42:53 +0200

    PatchOperationBuilder is now reusable as a delete patch, optionally

--- CloudCanvas.Shared/Utilities/PatchOperationBuilder.cs
+++ CloudCanvas.Shared/Utilities/PatchOperationBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using CloudCanvas.Shared.DTOs;
+using Microsoft.Azure.Amqp.Encoding;
 using Microsoft.Azure.Cosmos;
 using System.Collections.ObjectModel;
 
@@ -6,7 +7,7 @@ namespace CloudCanvas.Shared.Utilities
 {
     public static class PatchOperationBuilder
     {
-        public static IReadOnlyList<PatchOperation> For(PatchGalleryItemDTO metadata)
+        public static IReadOnlyList<PatchOperation> For(PatchGalleryItemDTO metadata, bool enableSoftDelte = false)
         {
             Validate.Object(metadata);
             var ops = new List<PatchOperation>();
@@ -20,6 +21,8 @@ namespace CloudCanvas.Shared.Utilities
                 ops.Add(PatchOperation.Add($"/description", metadata.Description));
             if(metadata.UserTags?.Count > 0) // keep 1 thumbnail until feature to manually generate new ones from GUI
                 ops.Add(PatchOperation.Add($"/userTags", metadata.UserTags));
+            if (enableSoftDelte && metadata.DeletedOn != null && metadata.DeletedOn > DateTimeOffset.MinValue)
+                ops.Add(PatchOperation.Add($"/deletedOn", metadata.DeletedOn));
             return new ReadOnlyCollection<PatchOperation>(ops);
         }
     }
