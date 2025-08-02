c02aa15d5283c4df81ae21331f8b0c0dac18bfe8
Adolphe <a-jhay@hotmail.com>
2025-08-03 01:13:32 +0200

    refactor gallery.js - now handles blob edits and deletes, but also updates the UI. Will migrate to TypeScript next

--- CloudCanvas.Web/wwwroot/js/gallery.js
+++ CloudCanvas.Web/wwwroot/js/gallery.js
@@ -1,6 +1,7 @@
-
-var gallery = document.querySelector('#GalleryInventory');
-var carousel = document.querySelector('.carousel-inner');
+var gallery = $('#GalleryInventory');
+var carousel = $('.carousel-inner'); 
+const api = "http://localhost:7071/api/photos/"; 
+var metadata;
 
 (async function main() {
     await axios(api)
@@ -9,84 +10,110 @@ var carousel = document.querySelector('.carousel-inner');
         metadata.forEach(async item => await configureModals(item));
     });
 })();
+
+async function fetchGalleryItems(url) {
     try {
-        const response = await fetch("http://localhost:7071/api/photos");
+        const response = await fetch(url);
         if (!response.ok) throw new Error(`Error: ${response.status}`);
         const metadata = await response.json();
-        updateGalleryItems(metadata);
-        configureEditModals(metadata);
+        return metadata;
     } catch (error) {
         // TODO: better error handling; alert, prompt for page reload or JS retry
         console.error("Failed to fetch metadata:", error.message);
     }
-})();
-
-function updateGalleryItems(metadata) {
-    metadata.forEach(metaCard => {
-        var oldCard = gallery.querySelector(`div:has(img[src="${metaCard.url}"])`);
-        var carouselItem = carousel.querySelector(`div:has(img[src="${metaCard.url}"])`);
-        if (oldCard != undefined) {
-            updateCardMetadata(oldCard, metaCard)
-        }
-        if (carouselItem != undefined) {
-            updateCarouselItemMetadata(carouselItem, metaCard);
-        }
-    });
 }
 
-function updateGalleryItemByIdentifier(metadata) {
-    metadata.forEach(metaCard => {
-        var oldCard = gallery.querySelector(`#${metaCard.identifier}`);
-        var carouselItem = carousel.querySelector(`div:has(img[src="${metaCard.url}"])`);
-        if (oldCard != undefined) {
-            updateCardMetadata(oldCard, metaCard)
-        }
-        if (carouselItem != undefined) {
-            updateCarouselItemMetadata(carouselItem, metaCard);
-        }
-    });
-}
-
-function updateCardMetadata(viewCard, cardMeta, withFooter = true) {
-    var title = viewCard.querySelector('.card-title');
-    title.textContent = cardMeta.originalFilename || 'No Title';
-
-    var description = viewCard.querySelector('.card-text');
-    description.textContent = cardMeta.description || 'No Description';
-
-    if (withFooter) {
-        var footer = viewCard.querySelector('.card-footer');
-        footer.textContent = cardMeta.userId || 'Anonymous User';
+async function saveGalleryItem(item) {
+    try {
+        const response = await fetch(url + item.id, "patch", item);
+        if (!response.ok) throw new Error(`Error: ${response.status}`);
+        const metadata = await response.json();
+        return metadata;
+    } catch (error) {
+        // TODO: better error handling; alert, prompt for page reload or JS retry
+        console.error("Failed to fetch metadata:", error.message);
     }
 }
 
-function updateCarouselItemMetadata(viewCard, cardMeta) {
-    var title = viewCard.querySelector('.carousel-title');
-    title.textContent = cardMeta.originalFilename || 'No Title';
-
-    var description = viewCard.querySelector('.carousel-description');
-    description.textContent = cardMeta.description || 'No Description';
+async function deleteItemAsync(item) {
+    var id = item.id;
+    var userId = item.userId;
+    var request = { method: "delete", body: { id: id, userId: userId } };
+    const response = await fetch(api + id, request);
+    if (!response.ok) throw new Error(`Error: ${response.status}`);
 }
 
-function configureEditModals(metadata) {
-    metadata.forEach(blob => {
-        const modal = document.getElementById(`${blob.identifier}-edit-gallery-modal`)
-        if (modal) {
-            modal.addEventListener('show.bs.modal', event => {
-                // Button that triggered the modal
-                const button = event.relatedTarget
-                // Extract info from data-bs-* attributes
-                const recipient = button.getAttribute('data-bs-whatever')
-                // If necessary, you could initiate an Ajax request here
-                // and then do the updating in a callback.
+function updateTableRow(item) {
+    var tr = $(`#tr_${item.id}`);
+    var displayName = tr.find('.tr-displayName');
+    displayName.text(item.displayName ?? displayName.text());
 
-                // Update the modal's content.
-                const modalTitle = modal.querySelector('.modal-title')
-                const modalBodyInput = modal.querySelector('.modal-body input')
+    var description = tr.find('.tr-description');
+    description.text(item.description ?? description.text());
+}
 
-                modalTitle.textContent = `New message to ${recipient}`
-                modalBodyInput.value = recipient
-            })
+function configureModals(item) {
+    var deleteBtn = $(`#Del-${item.id} .btn-delete-confirm`);
+    deleteBtn.on('click', async (btn) => {
+        var dataset = collectDataset(btn);
+        await deleteItemAsync(dataset);
+        deleteTableRow(`tr_${dataset.id}`);
+        deleteCarouselItem(item);
+    });
+
+    var saveChangesBtn = $(`#Mod-${item.id} .btn-edit-item`);
+    saveChangesBtn.on("click", async (btn) => {
+        var set = collectDataset(btn);
+        var payload = buildPayload(set);
+        var res = await axios.patch(api + set.id, payload);
+        if (res) {
+            item.displayName = res.data.displayName;
+            item.description = res.data.description;
+            updateTableRow(item);
+            updateCarouselItem(item);
         }
     });
 }
+
+function buildPayload(dataset) {
+    return {
+        "id": dataset.id,
+        "userId": dataset.userId,
+        "displayName": $(`#title-${dataset.id}`).val(),
+        "description": $(`#caption-${dataset.id}`).val()
+    }
+}
+
+function collectDataset(btn) {
+    return {
+        "id": btn.currentTarget.dataset.identifier,
+        "userId" : btn.currentTarget.dataset.userId
+    }
+}
+
+function deleteTableRow(tr_id) {
+    var tr = document.getElementById(tr_id);
+    metadata = metadata.filter(item => "tr_" + item.id != tr_id);
+    if (tr) tr.remove();
+}
+
+function deleteCarouselItem(item) {
+    var carouselItem = carousel.find(`div:has(img[src="${item.url}"])`);
+    if (carouselItem) carouselItem.remove();
+}
+
+function updateCarousel(metadata) {
+    metadata.forEach(item => updateCarouselItem(item));
+}
+
+function updateCarouselItem(item) {
+    var carouselItem = carousel.find(`div:has(img[src="${item.url}"])`);
+    if (carouselItem != undefined) {
+        var title = carouselItem.find('.carousel-title');
+        title.textContent = item.originalFilename || 'No Title';
+
+        var description = carouselItem.find('.carousel-description');
+        description.textContent = item.description || 'No Description';
+    }
+}
+
