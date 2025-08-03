d203fb4dde14086911b0831f6a9afbe0efa0b05c
Adolphe <a-jhay@hotmail.com>
2025-08-03 02:21:12 +0200

    Implemented full CRUD operations Blobs Function API

--- Functions/APIs/BlobStorageApi/BlobsApiFunction.cs
+++ Functions/APIs/BlobStorageApi/BlobsApiFunction.cs
@@ -4,11 +4,14 @@ using CloudCanvas.Shared.Exceptions;
 using CloudCanvas.Shared.Interfaces;
 using CloudCanvas.Shared.Services;
 using CloudCanvas.Shared.Utilities;
+using Grpc.Core;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Runtime.CompilerServices;
 
 namespace BlobStorageApi;
 
@@ -25,19 +28,45 @@ public class BlobsApiFunction
         _bservice = blobStorageService;
     }
 
-    [Function("ListBlobs")]
+    [Function(name: "GetAllBlobs")]
     public async Task<IActionResult> Get([HttpTrigger(AuthorizationLevel.Function, "get", Route = "photos")] HttpRequest req)
     {
-        const string _container = CloudCosmos.Containers.BlobMeta;
-        _logger.LogInformation("[Http:{req.Method}] Incoming request at '/api/photos'... Fetching metadata from cosmos container '{container}'...", req.Method, _container);
-        var res = await _cosmos.ListBlobsAsync<GalleryItemDTO>(_container);
-        return new OkObjectResult(res);
+        try
+        {
+            const string _container = CloudCosmos.Containers.BlobMeta;
+            _logger.LogInformation("Incoming {red.Method} request at '{route}'. Using container '{container}'.", req.Method, req.RouteValues, _container);
+            var res = await _cosmos.ListBlobsAsync<GalleryItemDTO>(_container);
+            return new OkObjectResult(res);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Failed to get metadata. Method: {req.Method}, Message: {msg}", req.Method, e.Message);
+            return new UnprocessableEntityObjectResult(new { error = e.Message });
+        }
     }
 
-    [Function("SaveBlob")]
+
+    [Function(name: "GetSingleBlob")]
+    public async Task<IActionResult> GetSingle([HttpTrigger(AuthorizationLevel.Function, "get", Route = "photos/{identifier}")] HttpRequest req, string identifier, string userId)
+    {
+        try
+        {
+            const string _container = CloudCosmos.Containers.BlobMeta;
+            _logger.LogInformation("Incoming {red.Method} request at '{route}'. Using container '{container}'.", req.Method, req.RouteValues, _container);
+            var res = await _cosmos.SingleAsync<GalleryItemDTO>(identifier, userId, _container);
+            return new OkObjectResult(res);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Failed to get metadata. Method: {req.Method}, Message: {msg}", req.Method, e.Message);
+            return new UnprocessableEntityObjectResult(new { error = e.Message });
+        }
+    }
+
+    [Function(name: "SaveBlob")]
     public async Task<IActionResult> Post([HttpTrigger(AuthorizationLevel.Function, "post", "put", Route = "photos")] HttpRequest req)
     {
-        _logger.LogInformation("[Http:{req.Method}] Incoming request at '{route}'... Verifying & Saving metadata...", req.Method, req.RouteValues);
+        _logger.LogInformation("Incoming {req.Method} Request at '{route}'.", req.Method, req.RouteValues);
         using var reader = new StreamReader(req.Body);
         var payload = await reader.ReadToEndAsync();
         bool overwrite = req.Method == "PUT";
@@ -45,20 +74,20 @@ public class BlobsApiFunction
         {
             var meta = CCSerializer.Deserialize<GalleryItemDTO>(payload); // Validation behind the scenes
             var res = await _cosmos.SaveMetadataAsync(meta, CloudCosmos.Containers.BlobMeta, overwrite);
-            _logger.LogInformation("[Http:{req.Method}] Successfully saved metadata for blob '{identifier}' to container . Signing off...", req.Method, res.Id); // meta.Id always == identifier (or meta.Name)
-            return new OkObjectResult(res);
+            _logger.LogInformation("Received {req.Method} Saved metadata for blob '{identifier}'.", req.Method, res.Id); // meta.Id always == identifier (or meta.Name)
+            return new OkObjectResult(new { StatusCode = HttpStatusCode.Created, Value = res });
         }
         catch (CCSerializationException e)
         {
-            _logger.LogError(e, "[Http:{req.Method}] ERROR: Deserialization of incoming request.body failed with message:\n{msg}", req.Method, e.Message);
+            _logger.LogError(e, "Deserialization of {req.Method} payload failed with message: {msg}", req.Method, e.Message);
             return new UnprocessableEntityObjectResult(new { error = e.Message });
         }
     }
 
-    [Function("PatchBlobMetadata")]
+    [Function(name: "PatchBlobMetadata")]
     public async Task<IActionResult> Patch([HttpTrigger(AuthorizationLevel.Function, "patch", Route = "photos/{identifier}")] HttpRequest req, string identifier)
     {
-        _logger.LogInformation("[Http:{req.Method}] Request to patch blob/file '{identifier}'. Verifying data, then Applying changes...", req.Method, identifier);
+        _logger.LogInformation("{req.Method} Request for blob '{identifier}'.", req.Method, identifier);
         using var reader = new StreamReader(req.Body);
         var stream = await reader.ReadToEndAsync();
         try
@@ -67,42 +96,48 @@ public class BlobsApiFunction
             var ops = PatchOperationBuilder.For(dto);
             if (ops.Any())
             {
-                dto = await _cosmos.PatchItemAsync(identifier, dto.UserId, ops);
-                _logger.LogInformation("[Http:{req.Method}] Successfully processed patch for blob/file '{identifier}'. Signing off...", req.Method, identifier);
-                return new ObjectResult(dto);
+                string container = CloudCosmos.Containers.BlobMeta;
+                dto = await _cosmos.PatchItemAsync<PatchGalleryItemDTO>(identifier, dto.UserId, container, ops);
+                _logger.LogInformation("Carried out {operationCount} {req.Method} operations for blob '{containerName}/{identifier}'", ops.Count(), req.Method, container, identifier);
+                return new OkObjectResult(dto);
             }
-            _logger.LogInformation("[Http:{req.Method}] Change-list empty for '{identifier}'. False alarm, going back to sleep.", req.Method, identifier);
-            return new BadRequestObjectResult("Change list is empty.");
+            _logger.LogInformation("Patch-list empty for '{identifier}'. {req.Method} Request ignored.", identifier,  req.Method);
+            return new BadRequestObjectResult(new { StatusCode = HttpStatusCode.Created, Value = "Patch-list is empty. Please make/add at least one valid change." });
         } catch (Exception e) when (e is CosmosContainerNotFoundException || e is CCSerializationException)
         {
-            _logger.LogError("[Http:{req.Method}] Failed to patch blob/file '{identifier}'.", req.Method, identifier);
-            return new BadRequestObjectResult("Failed to update this gallery item. Please refresh and try again."); // what should I return here?
+            _logger.LogError(e, "{req.Method} Request failed for blob '{identifier}'.", req.Method, identifier);
+            return new BadRequestObjectResult(new { StatusCode = HttpStatusCode.Created, Value = "Failed to update this gallery item. Please refresh and try again." }); // what should I return here?
         }
     }
 
 
-    [Function("SoftDeleteBlob")]
+    [Function(name: "SoftDeleteBlob")]
     public async Task<IActionResult> Delete([HttpTrigger(AuthorizationLevel.Function, "delete", Route = "photos/{identifier}")] HttpRequest req, string identifier)
     {
-        _logger.LogInformation("[Http:{req.Method}] Soft-delete requested for blob/file '{identifier}'. Applying to cosmos and blob storage...", req.Method, identifier);
-        var container = await _bservice.GetOrCreateContainerClientAsync(BlobStorage.Containers.Uploads, false);
-        var bclient = container.GetBlobClient(identifier);
-        var props = await bclient.GetPropertiesAsync();
-        BlobMetaDTO blob = CCSerializer.MetaFromBlobProperties(identifier, bclient.Uri.ToString(), props);
+        _logger.LogInformation("Soft-delete requested. Method: {req.Method}, blobId: '{identifier}'", req.Method, identifier);
+        BlobMetaDTO blob = await _bservice.GetBlobMetaAsync(identifier, BlobStorage.Containers.Uploads);
         blob.DeletedOn = DateTimeOffset.Now;
-        blob.Metadata[BlobStorage.Meta.DeletedOn] = blob.DeletedOn.ToString()!;
+        blob = await _bservice.AddMetadataAsync(blob, BlobStorage.Meta.DeletedOn, blob.DeletedOn.ToString()!);
+        using var reader = new StreamReader(req.Body);
+        var bodyString = await reader.ReadToEndAsync();
         try
         {
-            blob = await _cosmos.SaveMetadataAsync(blob, CloudCosmos.Containers.BlobMeta, true);
-            await bclient.SetMetadataAsync(blob.Metadata);
-            var blob_delete_success = await _bservice.DeleteAsync(container.Name, identifier);
-            _logger.LogInformation("[Http:{req.Method}] Soft-deletion carried out successfully: '{identifier}' deleted from {containerName} and {.", req.Method, identifier, container.Name);
-            return new OkObjectResult(204);
+            Validate.StringValue(nameof(bodyString), bodyString);
+            // first, soft-delete metadata, if that works then do the physical blob too
+            var patch = new PatchGalleryItemDTO
+            {
+                DeletedOn = blob.DeletedOn
+            };
+            var ops = PatchOperationBuilder.For(blob, true);
+            var patchedItem = await _cosmos.PatchItemAsync<PatchGalleryItemDTO>(blob.Id, blob.UserId, CloudCosmos.Containers.BlobMeta, ops);
+            var blob_delete_success = await _bservice.DeleteAsync(blob.ContainerName, identifier); // works regardless of blob's existence, but might throw on container 404
+            _logger.LogInformation("Soft-deleted blob '{identifier}' from {containerName}", identifier, blob.ContainerName);
+            return new OkObjectResult(new { StatusCode = 204, Value = "Not Found"});
         }
-        catch (Exception e) when (e is InvalidArgumentException || e is ArgumentNullException)
+        catch (Exception e) when (e is CCSerializationException || e is InvalidArgumentException || e is ArgumentNullException)
         {
-            _logger.LogWarning(e, "[Http:{req.Method}] Failed to delete blob '{identifier}' from container '{containerName}'.", req.Method, identifier, container.Name);
-            return new NotFoundObjectResult($"Oops! This gallery item can no longer be found.");
+            _logger.LogWarning(e, "{req.Method} Request Failed: soft-delete blob '{identifier}' from container '{containerName}'", req.Method, identifier, blob.ContainerName);
+            return new NotFoundObjectResult(new { StatusCode = 204, Value = "Not Found" });
         }
     }
 }
