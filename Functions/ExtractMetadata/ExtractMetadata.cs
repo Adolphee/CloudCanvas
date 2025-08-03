65e73ccba73667a24413262740f038e696d7060e
Adolphe <a-jhay@hotmail.com>
2025-08-03 02:20:11 +0200

    improved observability & chopped monolithic azure function into smaller code blocks

--- Functions/ExtractMetadata/ExtractMetadata.cs
+++ Functions/ExtractMetadata/ExtractMetadata.cs
@@ -1,9 +1,11 @@
+using Azure.Messaging.ServiceBus;
 using CloudCanvas.Shared.Constants;
 using CloudCanvas.Shared.DTOs;
 using CloudCanvas.Shared.Enums;
 using CloudCanvas.Shared.Interfaces;
 using CloudCanvas.Shared.Services;
 using CloudCanvas.Shared.Utilities;
+using Grpc.Core;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 namespace CloudCanvas.Functions;
@@ -21,33 +23,45 @@ public class ExtractMetadata(ILogger<ExtractMetadata> logger, BlobStorageService
     /// <remarks>This method is triggered by a blob upload to the specified container. It extracts metadata
     /// from the blob, logs the processing details, and sends a message to the Service Bus topic for further
     /// processing.</remarks>
-    /// <param name="input">The stream representing the uploaded blob content.</param>
-    /// <param name="name">The name of the uploaded blob.</param>
+    /// <param identifier="input">The stream representing the uploaded blob content.</param>
+    /// <param identifier="identifier">The identifier of the uploaded blob.</param>
     /// <returns>A <see cref="ServiceBusMessageDTO"/> containing the event details, subject, and extracted metadata.</returns>
     [Function(nameof(ExtractMetadata))]
-    public async Task Run([BlobTrigger(BlobStorage.Containers.Uploads + "/{name}", Connection = Secrets.MNSTRG)] Stream input, string name)
+    public async Task Run([BlobTrigger(BlobStorage.Containers.Uploads + "/{identifier}", Connection = Secrets.MNSTRG)] Stream input, string identifier)
     {
-        Validate.StringValue(nameof(name), name); // No use moving forward withouth is name
-        _logger.LogInformation("[START] Function Start - Extracting metadata from blob: {name}", name);
+        Validate.StringValue(nameof(identifier), identifier); // No use moving forward withouth is identifier
+        _logger.LogInformation("Function Start - Extracting metadata from blob: {identifier}", identifier);
         const string uploads = BlobStorage.Containers.Uploads;
         var bcClient = await _blobSerivce.GetOrCreateContainerClientAsync(uploads);
-        var blob = bcClient.GetBlobClient(name); // There is already validation on name etc from the SDK
+        var blob = bcClient.GetBlobClient(identifier); // There is already validation on identifier etc from the SDK
+
+        try
+        {
+            BlobMetaDTO metadata = CCSerializer.MetaFromBlobProperties(identifier, blob.Uri.ToString(), blob.GetProperties());
+            if (!await _cosmos.MetaExistsAsync(CloudCosmos.Containers.BlobMeta, metadata.Id, metadata.UserId))
+            {
+                metadata = await _cosmos.SaveMetadataAsync(metadata, CloudCosmos.Containers.BlobMeta);
+            }
+            var message = BuildSBMessage(metadata);
+            var responseMessageId = await _sbAdapter.SendAsync(ServiceBus.Topics.FileUpdates, message); // Send the message and call it a day
+            _logger.LogInformation("{correlationId} Sent Message '{messageId}' to topic '{topic}': {subject}", message.CorrelationId, responseMessageId, ServiceBus.Topics.FileUpdates, message.Subject);
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+        _logger.LogInformation("Extracted Metadata from blob: {identifier}", identifier);
         
-        BlobMetaDTO metadata = CCSerializer.MetaFromBlobProperties(name, blob.Uri.ToString(), blob.GetProperties());
-        metadata.Name = name;
-        metadata.ContainerName = BlobStorage.Containers.Uploads;
-        metadata.ProcessingStage = (int) BlobProcessingStage.ExtractMetadata;
-        metadata.Project = "CloudCanvas"; // TODO: This should be dynamic, based on the blob name or metadata
-        metadata = await _cosmos.SaveMetadataAsync(metadata, CloudCosmos.Containers.BlobMeta);
-        _logger.LogInformation("[OK] Extracted Metadata from blob: {name}. Sending Message...", name);
-        
-        var message = MessageFactory.BuildFor(metadata) // Manual dispatch required for full control (dotnet-isolated)
-            .WithSubject($"{ServiceBus.Status.MetadataExctracted} - file ready for processing.")    // Add Subject
+    }
+
+    private ServiceBusMessage BuildSBMessage(BlobMetaDTO metadata)
+    {
+        return MessageFactory.BuildFor(metadata) // Manual dispatch required for full control (dotnet-isolated)
+            .WithSubject($"{ServiceBus.Status.NewBlobDetected} - file ready for processing")    // Add Subject
             .AddProperty(ServiceBus.Props.EventType, ServiceBus.Subs.ExtractMetaData) // So that it makes it through subscription filters
-            .AddProperty(ServiceBus.Props.ThumbnailSize, (int) ThumbnailSize.small) // BuildFor thumbnail generation, later used by orchestrators to fan-out differet sizes
+            .AddProperty(ServiceBus.Props.ThumbnailSize, (int)ThumbnailSize.small) // BuildFor thumbnail generation, later used by orchestrators to fan-out differet sizes
             .SetCorrelationId(Guid.NewGuid().ToString()) // Set a new CorrelationId for this message, as the first in the chain
             .Finalize(); // Finalize and return the message ---> TODO: add custom (meaningful & descriptive) message ID builder, like {eventType}-{blobName}-{timestamp}
-        var responseMessageId = await _sbAdapter.SendAsync(ServiceBus.Topics.FileUpdates, message); // Send the message and call it a day
-        _logger.LogInformation("[DONE] Sent Message '{messageId}' to topic '{topic}': {subject}", responseMessageId, ServiceBus.Topics.FileUpdates, message.Subject);
     }
 }
