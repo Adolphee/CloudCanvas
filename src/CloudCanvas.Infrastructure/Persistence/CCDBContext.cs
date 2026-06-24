4a7539bcd7dc1534f4b418bed41df79c613b49e8
Adolphe <a-jhay@hotmail.com>
2026-06-24 14:55:34 +0200

    Refactor: reset and consolidate EF Core migrations
    Major migration reset: move migrations to Infrastructure, remove legacy migrations, update namespaces, switch to string PKs, clarify relationships, and align schema with new domain-driven design. Cleans up and simplifies the model and context configuration.

--- src/CloudCanvas.Infrastructure/Persistence/CCDBContext.cs
+++ src/CloudCanvas.Infrastructure/Persistence/CCDBContext.cs
@@ -12,31 +12,35 @@ public class CCDBContext(DbContextOptions<CCDBContext> options) : IdentityDbCont
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        modelBuilder.Entity<ApplicationUser>().ToTable("AspNetUsers")
-            .HasMany<Post>().WithOne().OnDelete(DeleteBehavior.NoAction);
+        modelBuilder.Entity<ApplicationUser>(u => {
+            u.HasMany<Gallery>().WithOne().HasConstraintName("SingleUser_ManyGalleries");
+        });
 
         modelBuilder.Entity<Post>(p =>
         {
             p.ToTable("Posts").UseTptMappingStrategy();
-            p.HasMany<Reaction>().WithOne(r => r.Post).HasForeignKey(r => r.PostId).OnDelete(DeleteBehavior.NoAction);
-            p.HasMany<Comment>().WithOne(c => c.Post).HasForeignKey(c => c.PostId).OnDelete(DeleteBehavior.NoAction);
+            p.HasOne<ApplicationUser>().WithMany(p => p.Posts)
+            .HasConstraintName("SingleUser_ManyPosts")
+            .OnDelete(DeleteBehavior.NoAction);
         });
 
         modelBuilder.Entity<Reaction>(r =>
         {
             r.ToTable("Reactions").UseTptMappingStrategy();
-            r.HasIndex(r => new { r.UserId, r.PostId, r.Type })
-            .IsUnique();
+            r.HasIndex(r => new { r.UserId, r.PostId, r.Type }).IsUnique();
+        });
+
+
+        modelBuilder.Entity<Photo>(p =>
+        {
+            p.ToTable("Photos");
+            p.HasOne<Gallery>().WithMany(g => g.Photos).HasForeignKey(p => p.GalleryId)
+            .HasConstraintName("SingleGallery_ManyPhotos")
+            .OnDelete(DeleteBehavior.NoAction);
         });
 
         modelBuilder.Entity<Comment>().ToTable("Comments");
-
         modelBuilder.Entity<PhotoThumbnail>().ToTable("PhotoThumbnails");
-
-        modelBuilder.Entity<Photo>().ToTable("Photos");
-
-
         modelBuilder.Entity<Gallery>().ToTable("Galleries");
-            
     }
 }
