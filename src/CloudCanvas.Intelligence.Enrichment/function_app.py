70a8a5516917f5416a44c8c2926cb04d0350ed8a
Adolphe <a-jhay@hotmail.com>
2026-08-16 11:16:28 +0200

    Refactor service bus topic trigger to improve message handling and ensure proper environment variable usage

--- src/CloudCanvas.Intelligence.Enrichment/function_app.py
+++ src/CloudCanvas.Intelligence.Enrichment/function_app.py
@@ -1,19 +1,28 @@
 import os
 import logging
 import azure.functions as func
+from infrastructure.vision_ai.generate_tags import generate_tags
 
-TOPIC_NAME = os.environ["SBTopicName"]
-SUBSCRIPTION_NAME = os.environ["SBSubscription"]
+TOPIC_NAME = str(os.environ.get("SBTopicName"))
+SUBSCRIPTION_NAME = str(os.environ.get("SBSubscription"))
 SBCONNECTION_STRING = "SBConnection"
+max_size = int(os.environ.get("MAX_MESSAGE_SIZE", 1024 * 1024))  # Default to 1 MB if not set
 
 app = func.FunctionApp()
 
+@app.function_name(name="generate_ai_tags")
 @app.service_bus_topic_trigger(
-    arg_name="msg",
+    aync=True,
+    arg_name="message",
     topic_name=TOPIC_NAME,
     subscription_name=SUBSCRIPTION_NAME,
     connection=SBCONNECTION_STRING,
-    is_sessions_enabled=True)
-def handle_topic_message(msg: func.ServiceBusMessage):
-    # Process the message
-    logging.info(f"Received message: {msg.get_body().decode('utf-8')}")
+    is_sessions_enabled=True, 
+    max_message_size=max_size
+)
+async def handle_topic_message(message: func.ServiceBusMessage):
+    image_url = message.get_body().decode('utf-8')
+    logging.info(f"Processing image URL: {image_url}")
+    tags = await generate_tags(image_url)
+    for tag in tags:
+        logging.info(f"Generated Tag: {tag}")
